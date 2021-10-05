9a935104d14da803e420b6761b70a64a30971bb2
zvnico <nicolas.baconnier@efrei.net>
2021-10-05 13:46:55 +0200

    tp1

--- src/tp1/Livre.java
+++ src/tp1/Livre.java
@@ -1,2 +1,49 @@
-package tp1;public class Livre {
+package tp1;
+
+public class Livre {
+    private String titre, auteur;
+    private int nbPages;
+
+    public static void main(String[] args) {
+        Livre livre1 = new Livre("J.K Rolling", "Harry Potter à l'école des sorciers");
+        Livre livre2 = new Livre("Adolf Hitler", "Mein Kampf");
+        livre1.setNbPages(5);
+        livre2.setNbPages(10);
+        System.out.printf("Auteur de '%s': %s\nAuteur de '%s': %s\n", livre1.getTitre(), livre1.getAuteur(), livre2.getTitre(), livre2.getAuteur());
+        System.out.printf("Nombre de pages de '%s': %d\nNombre de pages de '%s': %d\n", livre1.getTitre(), livre1.getNbPages(), livre2.getTitre(), livre2.getNbPages());
+        System.out.printf("Nombre de pages de '%s' + nombre de pages de '%s': %d\n", livre1.getTitre(), livre2.getTitre(), livre1.getNbPages() + livre2.getNbPages());
+    }
+
+    public Livre(String unAuteur, String unTitre) {
+        auteur = unAuteur;
+        titre = unTitre;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public int getNbPages() {
+        return nbPages;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public void setNbPages(int nbPages) {
+        if (nbPages >= 0) {
+            this.nbPages = nbPages;
+        } else {
+            System.out.println("Le nombre de pages passé en paramètre doit être positif pour modifie l'attribut");
+        }
+    }
 }
--- src/tp2/TestTableaux.java
+++ src/tp2/TestTableaux.java
@@ -1,2 +1,27 @@
-package tp2;public class TestTableaux {
+package tp2;
+
+public class TestTableaux {
+
+    public static void main(String[] args) {
+        int[] tab = new int[10];
+        for (int i = 0; i < 10; i++) {
+            tab[i] = i * 3;
+        }
+        afficher(tab);
+    }
+
+    public static boolean est_vide(int[] array) {
+        return array.length <= 0;
+    }
+
+    public static void afficher(int[] array) {
+        for (int value : array) {
+            System.out.print(value);
+        }
+        System.out.println();
+    }
+
+    public TestTableaux() {
+
+    }
 }
